0b983e5fb436dbf435f99feec1193dc79b4fc5af
Mübarek ALBAYATI <mubapc1@gmail.com>
2024-04-02 22:30:56 +0300

    first commit

---
+++ contentful-cms-project/src/data.js
@@ -0,0 +1,80 @@
+'Space ID - lf1tqdkeo58f'
+'Access Token - vIsl_neGfAdbbkLkxZGQAgfxtjRRrrF5QaCqtI2wn_k'
+
+const projects = [
+  {
+    title: 'birthday buddy',
+    url: 'https://react-birthday-buddy-m-albayati.netlify.app/',
+    image: './assets/birthday.png',
+  },
+  {
+    title: 'tours',
+    url: 'https://react-tours-m-albayati.netlify.app/',
+    image: './assets/tours.png',
+  },
+  {
+    title: 'reviews',
+    url: 'https://react-reviews-m-albayati.netlify.app/',
+    image: './assets/reviews.png',
+  },
+  {
+    title: 'questions',
+    url: 'https://react-accordion-m-albayati.netlify.app/',
+    image: './assets/questions.png',
+  },
+  {
+    title: 'menu',
+    url: 'https://react-menu-m-albayati.netlify.app/',
+    image: '',
+  },
+  {
+    title: 'tabs',
+    url: 'https://react-tabs-m-albayati.netlify.app/',
+    image: '',
+  },
+  {
+    title: 'slider',
+    url: 'https://react-slider-m-albayati.netlify.app/',
+    image: '',
+  },
+  {
+    title: 'lorem ipsum',
+    url: 'https://react-lorem-ipsum-m-albayati.netlify.app/',
+    image: '',
+  },
+  {
+    title: 'color generator',
+    url: 'https://react-color-generator-m-albayati.netlify.app/',
+    image: '',
+  },
+  {
+    title: 'grocery bud',
+    url: 'https://react-grocery-bud-m-albayati.netlify.app/',
+    image: '',
+  },
+  {
+    title: 'navbar',
+    url: 'https://react-navbar-m-albayati.netlify.app/',
+    image: '',
+  },
+  {
+    title: 'sidebar and modal',
+    url: 'https://react-sidebar-modal-m-albayati.netlify.app/',
+    image: '',
+  },
+  {
+    title: 'strapi menu',
+    url: 'https://react-strapi-menu-m-albayati.netlify.app/',
+    image: '',
+  },
+  {
+    title: 'cart',
+    url: 'https://react-cart-m-albayati.netlify.app/',
+    image: '',
+  },
+  {
+    title: 'unsplash images',
+    url: 'https://react-unsplash-m-albayati.netlify.app/',
+    image: '',
+  },
+]
